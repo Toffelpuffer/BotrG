7be6ab2aae3cf7d388e4961124f05563ef217d07
Toffelpuffer <Toffelpuffer1@gmx.de>
2018-10-04 19:15:34 +0200

    Added animation movement.

--- core/src/com/yourmom/animation/states/AnimStatePoisonDmg.java
+++ core/src/com/yourmom/animation/states/AnimStatePoisonDmg.java
@@ -3,6 +3,8 @@ package com.yourmom.animation.states;
 import com.yourmom.animation.BotrGAnimation;
 import com.yourmom.screenassets.BotrGSceneBattle;
 
+import static com.yourmom.game.BotrGStyle.screenWidth;
+
 /**
  * Created by Ben on 8/25/2018.
  */
@@ -11,12 +13,17 @@ public class AnimStatePoisonDmg extends BotrGAnimation {
     private boolean animActive;
     private int frameCnt;
     private int delay;
-    private final int ANIM_LENGTH = 100;
+    private boolean isPlayer;
+    private final int ANIM_LENGTH = 20;
+    private final float MOVELENGTH = (20.0f / 1440.0f) * screenWidth;
+    private final int MOVEFRAMES = 3;
 
-    public AnimStatePoisonDmg(){
+    public AnimStatePoisonDmg(boolean playAnimOnPlayer){
         animActive = false;
         frameCnt = 0;
         delay = 0;
+        isPlayer = playAnimOnPlayer;
+
     }
 
     @Override
@@ -32,6 +39,18 @@ public class AnimStatePoisonDmg extends BotrGAnimation {
         if(animActive && frameCnt >= 0){
 
         //TODO: add StatePoisonDmg animation
+            if(frameCnt < MOVEFRAMES ||
+                    (frameCnt >= MOVEFRAMES * 2 && frameCnt < MOVEFRAMES * 3) ||
+                    (frameCnt >= MOVEFRAMES * 4 && frameCnt < MOVEFRAMES * 5)){
+                battleScene.getChar(isPlayer).setXPos(
+                        battleScene.getChar(isPlayer).getXPos() - MOVELENGTH);
+            }
+            if((frameCnt >= MOVEFRAMES && frameCnt < MOVEFRAMES * 2) ||
+                    (frameCnt >= MOVEFRAMES * 3 && frameCnt < MOVEFRAMES * 4) ||
+                    (frameCnt >= MOVEFRAMES * 5 && frameCnt < MOVEFRAMES * 6)){
+                battleScene.getChar(isPlayer).setXPos(
+                        battleScene.getChar(isPlayer).getXPos() + MOVELENGTH);
+            }
 
             frameCnt++;
         } else
